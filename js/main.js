84c6e3e073c4cc2bcd5d05d5aae96716ee343cba
Julia Klubova <juliettano@yandex.ru>
2021-03-12 23:28:18 +0300

    fix: reviews section

--- js/main.js
+++ js/main.js
@@ -19,7 +19,14 @@ var reviewsSlider = new Swiper('.reviews-slider', {
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
+  effect: "coverflow",
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  spaceBetween: 7,
 });
