15cbc0f1ddf7a4509d9ed9c0e9da3b32aacab617
Julia Klubova <juliettano@yandex.ru>
2021-03-24 00:52:59 +0300

    fix: adress

--- send.php
+++ send.php
@@ -8,13 +8,15 @@ require 'phpmailer/Exception.php';
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+$adress = $_POST['adress'];
 
 // Формирование самого письма
 $title = "Новое обращение Best Tour Plan";
 $body = "
 <h2>Новое обращение</h2>
 <b>Имя:</b> $name<br>
-<b>Телефон:</b> $phone<br><br>
+<b>Телефон:</b> $phone<br>
+<b>Адрес электронной почты:</b> $adress<br><br>
 <b>Сообщение:</b><br>$message
 ";
 
