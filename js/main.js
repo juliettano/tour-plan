e7ccc53be9f9ff32e1ae3b4647beafe579dd87d3
Julia Klubova <juliettano@yandex.ru>
2021-03-26 20:32:58 +0300

    create: modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+$(document).ready(function() {
 var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
@@ -41,3 +42,21 @@ menuButton.addEventListener("click", function () {
   document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom-active');
 });
 
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $(".modal__close");
+modalButton.on("click", openModal);
+closeModalButton.on("click", closeModal);
+
+function openModal() {
+  var targetModal = $(this).attr("data-href");
+  $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+  $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+}
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass(".modal__overlay--visible");
+  modalDialog.removeClass(".modal__dialog--visible");
+}
+});
